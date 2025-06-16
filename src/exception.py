40def58b56f711adb49fc4b6bcbf2297a58a9297
Chirayu Kapoor <chirayukapoor2002@gmail.com>
2025-06-16 18:00:03 +0530

    commiting indingmodelnotebook

--- src/exception.py
+++ src/exception.py
@@ -3,7 +3,7 @@ import sys
 def error_message_details(error_message , error_details:sys):
     _,_,exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message  = 'Error occured in python script name [{0}] line number [{1}] error message [{2}]'.format(file_name,exc_tb.tb_lineno,str(error))
+    error_message  = 'Error occured in python script name [{0}] line number [{1}] error message [{2}]'.format(file_name,exc_tb.tb_lineno,str(error_message))
     return error_message
     
 
